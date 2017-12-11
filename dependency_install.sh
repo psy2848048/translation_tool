234bc513747a4f51f5355d0a3133a0610c325072
Bryan <junhang.lee@ciceron.me>
2017-12-12 00:41:14 +0900

    Dependency install

---
+++ dependency_install.sh
@@ -0,0 +1,15 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(pyenv init -)"' >> ~/.bash_profile
+
+source ~/.bash_profile
+
+pyenv install 3.5.2
+pyenv virtualenv 3.5.2 marocat
+pyenv activate marocat
