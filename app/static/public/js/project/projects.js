4b6e7a28f4e3abc5542f905c102e43b0adf743db
doonee <doonee@ciceron.me>
2017-12-28 12:00:00 +0900

    번역기내 댓글 기본포맷, 기타 마이너 업데이트

--- app/static/public/js/project/projects.js
+++ app/static/public/js/project/projects.js
@@ -1,25 +1,7 @@
 var PageScript = function () {
     var local = this;
-    this.preInits = function () {
-
-        setTimeout(function () {
-            var menu = '';
-
-            menu += '<ul id="ulProjectList" style="max-height:200px;overflow-x:hidden;overflow-y:auto;">';
-            menu += '<li>';
-            menu += '<a href="/static/front/project/project_view.html?project=1">└ 내픽뉴스 영문번역</a>';
-            menu += '</li>';
-            menu += '<li>';
-            menu += '<a href="/static/front/project/project_view.html?project=2">└ 내픽뉴스 중문번역</a>';
-            menu += '</li>';
-            menu += '</ul>';
-
-            $('#left_menu_area>li:nth-of-type(1)').append(menu);
-        }, 100);
-    };
-    this.btnEvents = function () {
-
-    };
+    this.preInits = function () {};
+    this.btnEvents = function () {};
     this.getProjects = function (lang) {
         $(document).ajaxStart(function () {
             $('#dvLoading2').show();
@@ -27,9 +9,59 @@ var PageScript = function () {
         $(document).ajaxComplete(function (event, request, settings) {
             $('#dvLoading2').hide();
         });
+        var jqxhr = $.get('/api/v1/users/1/projects/', function (data) {
+                console.log('/api/v1/users/1/projects/ : ', data);
+                // 좌측메뉴
+                var menu = '',
+                    list = '';
+                if (data != undefined && data.result != '') {
+                    menu += '<ul id="ulProjectList" style="max-height:200px;overflow-x:hidden;overflow-y:auto;">';
+                    $(data.result).each(function (idx, res) {
+                        menu += '<li>';
+                        menu += '<a href="/static/front/project/project_view.html?project=' + res.project_id + '">└ ' + res.project_name + '</a>';
+                        menu += '</li>';
+                    });
+                    menu += '</ul>';
+                }
+                // 본문리스트
+                if (data != undefined && data.result != '') {
+                    $(data.result).each(function (idx, res) {
+                        list += '<tr>';
+                        list += '   <td>' + parseInt(idx + 1) + '</td>';
+                        list += '   <td style="max-width:150px;display: inline-block;white-space: nowrap;overflow:hidden;vertical-align:middle;">';
+                        list += '       <a href="project_view.html?project=' + res.project_id + '">' + res.project_name + '</a>';
+                        list += '   </td>';
+                        list += '   <td>' + res.progress_percent + '</td>';
+                        list += '   <td>' + res.status + '</td>';
+                        list += '   <td style="max-width:150px;display: inline-block;white-space: nowrap;overflow:hidden;vertical-align:middle;">';
+                        list += res.client_company + ' ';
+                        if (res.clients != undefined) {
+                            $(res.clients).each(function (idx, clients) {
+                                list += ', ' + clients[idx];
+                            });
+                        }
+                        list += '   </td>';
 
-        var jqxhr = $.get("/api/v2/mypick/" + lang, function (data) {
-                console.log(data);
+                        var ko_res_time = '', ko_duration_date = '';
+                        if(res.create_time != ''){
+                            var dt1 = new Date(res.create_time);
+                            ko_res_time = GetStringDate(dt1);
+                        }
+                        if(res.create_time != ''){
+                            var dt2 = new Date(res.duration_date);
+                            ko_duration_date = GetStringDate(dt2);
+                        }
+
+                        list += '   <td>' + ko_res_time + '</td>';
+                        list += '   <td>' + ko_duration_date + '</td>';
+                        list += '</tr>';
+                    });
+                }
+
+                $('#left_menu_area>li:nth-of-type(1)').append(menu);
+                $('#listContents table tbody tr').after(list);
+                console.log('menu : ', menu);
+                console.log('list : ', list);
             })
             .done(function () {})
             .fail(function () {
