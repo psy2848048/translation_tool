2508741dd0f834ba64e53631de04103699130324
sssunho <sh293zz@gmail.com>
2018-01-16 17:07:25 +0900

    번역편집툴, 프로젝트, 문서 부분 수정

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -13,31 +13,28 @@ def sign_in():
     if None in [email, password]:
         return make_response(json.jsonify(result='Something Not Entered'), 460)
 
-    #: email&password 형식 검사
-    #: email로 꺼낸 사용자의 비밀번호와 입력된 비밀번호가 일치하는지 검사한다
-    #: 일치한다면 login_user에 user 정보를 넣는다
-
     #: 입력된 email의 사용자 찾기
     user = model.select_user_by_email(email)
-    uid = user.get_id()
 
     if not user:
         return make_response(json.jsonify(result='User does not exist'), 401)
     else:
         #: 존재하는 사용자라면 입력된 password가 맞는지 확인
-        # is_ok = model.verify_password(password, uid)
         is_ok = user.can_login(password)
 
-        if is_ok is True:#: 비밀번호가 일치한다면 login_user에 user 정보를 넣고, 로그인 완료!
+        #: 비밀번호가 일치한다면 login_user에 user 정보를 넣고, 로그인 완료!
+        if is_ok is True:
             login_user(user, remember=True)
             return make_response(json.jsonify(result="User '{}' signed in!".format(user.get_id())), 200)
         else:
             return make_response(json.jsonify(result="Password is wrong"), 401)
 
+
 @login_manager.user_loader
 def user_loader(uid):
     user = model.select_user_by_uid(uid)
     return user
+
 
 @login_required
 def sign_out():
@@ -45,3 +42,20 @@ def sign_out():
     print(current_user.id)
     logout_user()
     return make_response(json.jsonify(result="User '{}' signed out!".format(uid)), 200)
+
+
+def send_password_recovery_email():
+    email = request.form.get('email', None)
+
+    if not email:
+        return make_response(json.jsonify(result='Email Not Entered'), 460)
+
+
+def recover_password():
+    email = request.form.get('email', None)
+    auth_code = request.form.get('auth_code', None)
+    new_pwd = request.form.get('new_pwd', None)
+
+    if None in [email, auth_code, new_pwd]:
+        return make_response(json.jsonify(result='Email Not Entered'), 460)
+
--- app/docs/models.py
+++ app/docs/models.py
@@ -21,7 +21,7 @@ def select_doc_members(did, page, rows):
                               WHERE doc_id = :did AND dm.is_deleted = FALSE AND u.is_deleted = FALSE;"""), did=did).fetchone()
     total_cnt = res[0]
 
-    results = conn.execute(text("""SELECT dm.user_id, u.name, u.email, can_read, can_modify, can_delete
+    results = conn.execute(text("""SELECT dm.user_id as id, u.name, u.email, can_read, can_modify, can_delete
                                    FROM `marocat v1.1`.doc_members dm JOIN users u ON u.id = dm.user_id
                                    WHERE dm.doc_id = :did AND dm.is_deleted = FALSE AND u.is_deleted = FALSE
                                    LIMIT :row_count OFFSET :offset;"""), did=did, row_count=rows, offset=rows * (page - 1))
@@ -83,25 +83,28 @@ def delete_doc(did):
     d = Table('docs', meta, autoload=True)
 
     try:
+        #: 번역문댓글 - 번역문 - 원문순으로 삭제
+        conn.execute(text("""UPDATE `marocat v1.1`.trans_comments tc JOIN ( doc_trans_sentences ts, doc_origin_sentences os, docs d ) ON ( ts.id = tc.trans_id AND ts.origin_id = os.id AND os.doc_id = d.id)
+                            SET tc.is_deleted=TRUE, tc.update_time=CURRENT_TIMESTAMP
+                            WHERE doc_id = :did;
+                            UPDATE `marocat v1.1`.doc_trans_sentences ts JOIN ( doc_origin_sentences os, docs d ) ON ( ts.origin_id = os.id AND os.doc_id = d.id)
+                            SET ts.is_deleted=TRUE, ts.update_time=CURRENT_TIMESTAMP
+                            WHERE doc_id = :did;
+                            UPDATE `marocat v1.1`.doc_origin_sentences os JOIN docs d ON os.doc_id = d.id
+                            SET os.is_deleted=TRUE, os.update_time=CURRENT_TIMESTAMP
+                            WHERE doc_id = :did;"""), did=did)
+
+        #: 문서내 프로젝트 참가자 삭제
+        res = conn.execute(dm.update(dm.c.doc_id == did), is_deleted=True, update_time=datetime.utcnow())
+        if res.rowcount < 1:
+            trans.rollback()
+            return False
+
+        #: 문서 삭제
         res = conn.execute(d.update(d.c.id == did), is_deleted=True, update_time=datetime.utcnow())
         if res.rowcount != 1:
             trans.rollback()
             return False
-
-        res = conn.execute(dm.update(dm.c.doc_id == did), is_deleted=True, update_time=datetime.utcnow())
-        if res.rowcount != 1:
-            trans.rollback()
-            return False
-
-        conn.execute(text("""UPDATE `marocat v1.1`.trans_comments tc JOIN ( doc_trans_sentences ts, doc_origin_sentences os, docs d ) ON ( ts.id = tc.trans_id AND ts.origin_id = os.id AND os.doc_id = d.id)
-                            SET tc.is_deleted=TRUE, tc.update_time=CURRENT_TIMESTAMP
-                            WHERE doc_id = :did;"""), did=did)
-        conn.execute(text("""UPDATE `marocat v1.1`.doc_trans_sentences ts JOIN ( doc_origin_sentences os, docs d ) ON ( ts.origin_id = os.id AND os.doc_id = d.id)
-                            SET ts.is_deleted=TRUE, ts.update_time=CURRENT_TIMESTAMP
-                            WHERE doc_id = :did;"""), did=did)
-        conn.execute(text("""UPDATE `marocat v1.1`.doc_origin_sentences os JOIN docs d ON os.doc_id = d.id
-                            SET os.is_deleted=TRUE, os.update_time=CURRENT_TIMESTAMP
-                            WHERE doc_id = :did;"""), did=did)
 
         trans.commit()
         return True
