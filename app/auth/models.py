f12790b036d3e03be9e17dc2c4018200c4d68914
sssunho <sh293zz@gmail.com>
2018-01-16 18:47:15 +0900

    비밀번호 복구 구현중...

--- app/auth/models.py
+++ app/auth/models.py
@@ -1,7 +1,8 @@
 from sqlalchemy import Table, MetaData, func, text
-from app import db
+from app import db, common
 import traceback
 from flask_login import UserMixin
+
 
 class User(UserMixin):
     def can_login(self, password):
@@ -12,6 +13,7 @@ class User(UserMixin):
             return True
         else:
             return False
+
 
 def select_user_by_email(email):
     conn = db.engine.connect()
@@ -29,6 +31,7 @@ def select_user_by_email(email):
 
         return user
 
+
 def select_user_by_uid(uid):
     conn = db.engine.connect()
     res = conn.execute(text("""SELECT id, name, email, profile_photo FROM `marocat v1.1`.users WHERE id = :uid;"""), uid=uid).fetchone()
@@ -44,3 +47,33 @@ def select_user_by_uid(uid):
         user.profile_photo = res['profile_photo']
 
         return user
+
+
+def create_auth_code(email):
+    conn = db.engine.connect()
+    meta = MetaData(bind=db.engine)
+    t = Table('token', meta, autoload=True)
+
+    # 입력된 이메일이 가입된 회원인지 확인
+    res = conn.execute(text("""SELECT email FROM `marocat v1.1`.users WHERE email = :email;"""), email=email).fetchone()
+
+    if res is None:
+        msg = email + 'is not exist'
+        return None, False, msg
+
+    # 가입된 회원이라면 인증코드 만들고 DB에 저장
+    try:
+        authcode = common.create_token(email)
+        issue = email + '님이 비밀번호 복구 요청'
+        res = conn.execute(t.insert(), token=authcode, issue=issue)
+
+        if res.rowcount != 1:
+            msg = 'Something Wrong!'
+            return None, False, msg
+    except:
+        traceback.print_exc()
+        msg = 'Something Wrong!'
+        return None, False, msg
+
+    msg = 'OK'
+    return authcode, True, msg
--- app/auth/urls.py
+++ app/auth/urls.py
@@ -3,8 +3,15 @@ import app.auth.controllers as ctrl
 
 auth = Blueprint('auth', __name__)
 
-auth.add_url_rule('/local/signin', view_func=ctrl.sign_in, methods=['POST'])  # 로그인
-auth.add_url_rule('/local/signout', view_func=ctrl.sign_out, methods=['GET'])  # 로그아웃
+# 로그인
+auth.add_url_rule('/local/signin', view_func=ctrl.sign_in, methods=['POST'])
 
-auth.add_url_rule('/pwRecovery/request', view_func=ctrl.send_password_recovery_email, methods=['POST'])  # 비밀번호 복구 인증코드 이메일 보내기
-auth.add_url_rule('/pwRecovery', view_func=ctrl.recover_password, methods=['POST'])  # 비밀번호 복구
+# 로그아웃
+auth.add_url_rule('/local/signout', view_func=ctrl.sign_out, methods=['GET'])
+
+
+# 비밀번호 복구 인증코드 이메일 보내기
+auth.add_url_rule('/pwRecovery/sendCode', view_func=ctrl.send_password_recovery_email, methods=['POST'])
+
+# 비밀번호 복구
+auth.add_url_rule('/pwRecovery', view_func=ctrl.recover_password, methods=['POST'])
