b5478f4384f8b575927023f4652e30802e5380e0
sssunho <sh293zz@gmail.com>
2018-02-06 16:26:05 +0900

    1802061626

--- app/users/urls.py
+++ app/users/urls.py
@@ -9,3 +9,5 @@ users.add_url_rule('/me', view_func=ctrl.get_user_info, methods=['GET'])
 users.add_url_rule('/me/pwd', view_func=ctrl.change_password, methods=['PUT'])
 users.add_url_rule('/me/nickname', view_func=ctrl.change_nickname, methods=['PUT'])
 users.add_url_rule('/me/picture', view_func=ctrl.change_picture, methods=['PUT'])
+
+users.add_url_rule('/test', view_func=ctrl.test_picture, methods=['GET'])
