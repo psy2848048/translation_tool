24705e7e4d36fed2d83d1553930d0eacdfede59e
sssunho <sh293zz@gmail.com>
2018-02-08 14:23:42 +0900

    1802081423

--- app/users/urls.py
+++ app/users/urls.py
@@ -9,11 +9,8 @@ users.add_url_rule('/me', view_func=ctrl.get_profile, methods=['GET'])
 users.add_url_rule('/me/pwd', view_func=ctrl.change_password, methods=['PUT'])
 users.add_url_rule('/me/nickname', view_func=ctrl.change_nickname, methods=['PUT'])
 users.add_url_rule('/me/picture', view_func=ctrl.change_picture, methods=['PUT'])
+
 users.add_url_rule('/me/picture', view_func=ctrl.get_thumbnail, methods=['GET'])
 users.add_url_rule('/me/picture/origin', view_func=ctrl.get_thumbnail_original, methods=['GET'])
 
-
-#: 테스트용
 users.add_url_rule('/me/bye', view_func=ctrl.user_withdraw, methods=['DELETE'])
-users.add_url_rule('/picture', view_func=ctrl.test_picture, methods=['GET'])
-users.add_url_rule('/s3url', view_func=ctrl.test_s3_getobjectlink, methods=['GET'])
