7121f14aaa235572b01a5416a6c23c806b6d98a5
sssunho <sh293zz@gmail.com>
2018-02-09 14:17:15 +0900

    프로젝트 등록시 마감기한 포맷 수정

--- app/common.py
+++ app/common.py
@@ -97,7 +97,7 @@ def convert_datetime_4mysql(basedate):
     client에서 보내주는 datetime이 MySQL 포맷에 맞지 않기 때문에 포맷형식을 바꿔주는 함수입니다
     """
     formatfrom = "%a, %d %b %Y %H:%M:%S GMT"
-    formatto = "%Y-%m-%d %H:%M:%S:00"
+    formatto = "%Y-%m-%d %H:%M:%S"
     convertdate = datetime.strptime(basedate, formatfrom).strftime(formatto)
     return convertdate
 
