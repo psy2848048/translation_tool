6f928c90f65fba7c5f030ef01625af10b641fa30
root <root@ip-172-31-16-248.ap-northeast-1.compute.internal>
2018-02-12 06:36:05 +0000

    포트번호 443으로 수정

--- run.py
+++ run.py
@@ -8,5 +8,5 @@ key = './cert_key/pfx.mycattool_com.key'
 
 # http = WSGIServer(('0.0.0.0', 5000), app)
 # http.serve_forever()
-https = WSGIServer(('0.0.0.0', 5001), app, keyfile=key, certfile=cert)
+https = WSGIServer(('0.0.0.0', 443), app, keyfile=key, certfile=cert)
 https.serve_forever()
