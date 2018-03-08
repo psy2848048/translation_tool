51cc373e019eecbce901f5680888600ff89c09f0
sunny <sunny@ciceron-sgui-MacBook-Pro.local>
2018-03-08 10:20:28 +0900

    환경변수 PURPOSE에 따라 포트번호 설정하기

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from app import app
 from gevent.wsgi import WSGIServer
+import os
 
 #: HTTPS, SSL 설정
 cert = './cert_key/pfx.mycattool_com.crt'
 key = './cert_key/pfx.mycattool_com.key'
 
-# http = WSGIServer(('0.0.0.0', 5000), app)
-#https = WSGIServer(('0.0.0.0', 5001), app, keyfile=key, certfile=cert)
-https = WSGIServer(('0.0.0.0', 443), app, keyfile=key, certfile=cert)
+https = None
+if os.environ.get('PURPOSE') != 'PROD':
+    https = WSGIServer(('0.0.0.0', 5001), app)
+else:
+    https = WSGIServer(('0.0.0.0', 443), app, keyfile=key, certfile=cert)
 https.serve_forever()
+
+# http = WSGIServer(('0.0.0.0', 5000), app)
