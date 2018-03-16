a376b1a930265bd0931f85e433cd37fa15694351
sunny <sunny@ciceron-sgui-MacBook-Pro.local>
2018-03-16 15:18:45 +0900

    문서 다운로드 API 코드 정리

--- app/steem/urls.py
+++ app/steem/urls.py
@@ -4,4 +4,3 @@ import app.steem.controllers as ctrl
 steem = Blueprint('steem', __name__)
 
 steem.add_url_rule('/trans/request', view_func=ctrl.request_trans, methods=['POST'])
-steem.add_url_rule('/trans/post', view_func=ctrl.post_export_and_save, methods=['POST'])
