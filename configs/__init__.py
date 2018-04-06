5f8a492ff7de55bac1b37cf62e1e632799b15823
sssssunny <sunny@ciceron.me>
2018-04-06 14:38:14 +0900

    세션, 로그아웃 문제
    세션 유지 시간말고 쿠키 유지 시간을 수정하였다.

--- configs/__init__.py
+++ configs/__init__.py
@@ -21,8 +21,8 @@ class Config(object):
     #: Session
     SESSION_TYPE = 'redis'
     SESSION_COOKIE_NAME = "MyCatToolCookie"
-    REMEMBER_COOKIE_DURATION = timedelta(hours=3)
-    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+    REMEMBER_COOKIE_DURATION = timedelta(days=15)
+    # PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
     SECRET_KEY = os.urandom(24)
 
     #: Swagger
